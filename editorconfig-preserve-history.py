fb7c753033fce388392373aa7331741d85657fbe
David Meybohm <dmeybohm@gmail.com>
2018-02-19 22:36:57 -0500

    Update

--- editorconfig-preserve-history.py
+++ editorconfig-preserve-history.py
@@ -5,9 +5,14 @@ import os
 import Change
 import re
 import sys
+import tempfile
 from editorconfig import get_properties, EditorConfigError
 
 commits = {}
+
+
+class RuntimeException(BaseException):
+    pass
 
 
 def run(cmd):
@@ -16,23 +21,65 @@ def run(cmd):
     return output.split("\n")
 
 
-def generate_changes(editorconfigConfig, file):
-    end_of_line = editorconfigConfig['end_of_line']
+def get_contents(filepath):
+    with open(file, "r") as f:
+        return f.read()
+
+def get_lines(filepath):
+    with open(file, "r") as f:
+        return f.readlines()
+
+
+def store_changes(file, contents, newcontents):
     blame = run(['git', 'blame', file])
+    for line_number, line in enumerate(blame):
+        if line == "":
+            continue
+        print("Line: "+line)
+        match = re.match(r'^(\S+) ', line)
+        if not match:
+            print("Bad match:" +str(len(line)))
+
+            raise RuntimeException("Bad match in git blame")
+        group = match.lastgroup
+        print(group)
+
+
+def generate_changes(editorconfigConfig, file):
+    contents, newcontents = run_editorconfig_changes(editorconfigConfig, file)
+    print("file: "+file+": "+str(len(newcontents)))
+    if newcontents == contents:
+        # no changes:
+        return
+    store_changes(file, contents, newcontents)
+
+
+def run_editorconfig_changes(editorconfigConfig, file, lines_to_change={}):
+    end_of_line = editorconfigConfig['end_of_line']
+    trim_trailing_whitespace = editorconfigConfig['trim_trailing_whitespace']
+    insert_final_newline = editorconfigConfig['insert_final_newline']
     if end_of_line == "lf":
-        repl = '\n'
+        eol = '\n'
     elif end_of_line == "crlf":
-        repl = '\r\n'
+        eol = '\r\n'
     else:
-        raise "Unhandled line ending"
-    with open(file, "r") as f:
-        contents = f.read()
-    print("contents: "+contents)
-    with open(file, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            new_line = re.sub(r"\r?\n$", repl, line)
-            sys.stdout.write(new_line)
+        raise RuntimeException("Unhandled line ending")
+    contents = get_contents(file)
+    lines = get_lines(file)
+    tmp = tempfile.TemporaryFile()
+    lastline = len(lines) - 1
+    for line_number, line in enumerate(lines):
+        # Do whitespace first to not strip carriage returns:
+        if trim_trailing_whitespace:
+            line = re.sub(r'\s*\n', '\n', line)
+        line = re.sub(r'\r?\n', eol, line)
+        if line_number == lastline and insert_final_newline and '\n' not in line:
+            line += eol
+        if not lines_to_change or lines_to_change[line_number]:
+            tmp.write(line)
+    tmp.seek(0, 0)
+    newcontents = tmp.read()
+    return contents, newcontents
 
 
 files = run(['git', 'ls-files'])
