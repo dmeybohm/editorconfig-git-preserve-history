8bc9e27a22897fa7b8c74dd73b15413117132fb3
David Meybohm <dmeybohm@gmail.com>
2018-02-24 10:52:55 -0500

    Fix deprecation warning

--- test_gitcommit.py
+++ test_gitcommit.py
@@ -21,27 +21,27 @@ Fix gitinfo.php
 class GitCommitTest(unittest.TestCase):
     def test_match_commit(self):
         commit = gitcommit.match_commit(test_commit)
-        self.assertEquals("9471e20cebc84f5931c77eb2bf6c8eb004ed2305", commit)
+        self.assertEqual("9471e20cebc84f5931c77eb2bf6c8eb004ed2305", commit)
 
     def test_match_author_at_beginning(self):
         author = gitcommit.match_author("Author: Cheese")
-        self.assertEquals("Cheese", author)
+        self.assertEqual("Cheese", author)
 
     def test_match_author_after_something(self):
         author = gitcommit.match_author("SOmething\nAuthor: Cheese")
-        self.assertEquals("Cheese", author)
+        self.assertEqual("Cheese", author)
 
     def test_match_author_in_middle(self):
         author = gitcommit.match_author(test_commit)
-        self.assertEquals("A User <user@example.org>", author)
+        self.assertEqual("A User <user@example.org>", author)
 
     def test_match_date(self):
         date = gitcommit.match_date(test_commit)
-        self.assertEquals('Tue Feb 20 00:52:25 2018 -0500', date)
+        self.assertEqual('Tue Feb 20 00:52:25 2018 -0500', date)
 
     def test_match_message(self):
         message = gitcommit.match_message(test_commit)
-        self.assertEquals(commit_message, message)
+        self.assertEqual(commit_message, message)
 
 
 if __name__ == "__main__":
