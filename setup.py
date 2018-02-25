bd6101fa82c983b3aacae6be3014ad72a933e43a
David Meybohm <dmeybohm@gmail.com>
2018-02-25 13:05:44 -0500

    Move code to submodule

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='editorconfig_git_preserve_history',
+      version='0.1',
+      description='Commit changes to editorconfig and preserve authorship',
+      url='http://github.com/dmeybohm/editorconfig-git-preserve-history',
+      author='David Meybohm',
+      author_email='dmeybohm@gmail.com',
+      license='MIT',
+      packages=['editorconfig_git_preserve_history'],
+      install_requires=[
+          'EditorConfig>=0.12.1',
+      ])
