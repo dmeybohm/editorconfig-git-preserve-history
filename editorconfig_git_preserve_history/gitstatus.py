7ac79831b6d731b875206a3b0a568936cbc39847
David Meybohm <dmeybohm@gmail.com>
2018-02-25 21:12:42 -0500

    Add gitstatus.py

---
+++ editorconfig_git_preserve_history/gitstatus.py
@@ -0,0 +1,11 @@
+from .util import run
+
+
+def haschanges() -> bool:
+    lines = run(['git', 'status', '--porcelain'])
+    for line in lines:
+        if line.startswith(' '):
+            line = line[1:]
+        if line[0] == 'M' or line[0] == 'A' or line[0] == 'D':
+            return True
+    return False
