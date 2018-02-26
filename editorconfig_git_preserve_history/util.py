1907abff796ebd6ca7c482cf2a3236fe57b4d4d5
David Meybohm <dmeybohm@gmail.com>
2018-02-25 21:25:33 -0500

    Check for empty strings

--- editorconfig_git_preserve_history/util.py
+++ editorconfig_git_preserve_history/util.py
@@ -9,7 +9,7 @@ def run(cmd: List[str], encoding: str = None) -> List[str]:
     if encoding is None:
         encoding = locale.getpreferredencoding()
     output_str = output.decode(encoding)
-    if output_str[-1] == '\n':
+    if len(output_str) > 0 and output_str[-1] == '\n':
         output_str = output_str[:-1]
     return output_str.split("\n")
 
