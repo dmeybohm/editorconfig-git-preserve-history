5e056ee5e0eaabe2bf8a51c61d6154e23acca958
David Meybohm <dmeybohm@gmail.com>
2018-02-25 21:13:17 -0500

    Clone whole repo for test

--- .travis-scripts/clone-test.sh
+++ .travis-scripts/clone-test.sh
@@ -18,7 +18,7 @@ EOF
 # Test installing the package with pip-install and then
 # running it on a previous version of its own repository
 pip install -e .
-git clone . clonetest 
+git clone https://github.com/dmeybohm/editorconfig-git-preserve-history clonetest 
 cd clonetest 
 git checkout -b clone-test 153c40eae86411b674e95235b4a66c8b4ee16024 
 for i in $(git ls-files)
